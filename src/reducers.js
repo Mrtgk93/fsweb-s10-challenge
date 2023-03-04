a78adbb150c626d104b558cb40986e8d895f485f
Mrtgk93 <mrtgk93@hotmail.com>
2023-03-04 03:11:59 +0300

    ekleme cıkarma calısıyor

--- src/reducers.js
+++ src/reducers.js
@@ -1,3 +1,5 @@
+import { BASLANGIC_DEGER, NOT_EKLE, NOT_SIL } from "./actions";
+
 const s10chLocalStorageKey = "s10ch";
 
 const baslangicDegerleri = {
@@ -11,19 +13,51 @@ const baslangicDegerleri = {
 };
 
 function localStorageStateYaz(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
+  localStorage.setItem(s10chLocalStorageKey, JSON.stringify(data));
 }
 
 function localStorageStateOku(key) {
-  return JSON.parse(localStorage.getItem(key));
+  return JSON.parse(localStorage.getItem(s10chLocalStorageKey));
 }
 
 function baslangicNotlariniGetir(key) {
-  const eskiNotlar = localStorage.getItem(key);
+  const eskiNotlar = localStorage.getItem(s10chLocalStorageKey);
 
   if (eskiNotlar) {
     return localStorageStateOku(key);
   } else {
-    return baslangicDegerleri
+    return baslangicDegerleri;
+  }
+}
+
+export function alfred(state = baslangicDegerleri, action) {
+  switch (action.type) {
+    case NOT_EKLE:
+      let newArr = {
+        ...state,
+        notlar: [...state.notlar, action.payload],
+      };
+      localStorageStateYaz(s10chLocalStorageKey, newArr.notlar);
+
+      return {
+        ...state,
+        notlar: [...state.notlar, action.payload],
+      };
+    case NOT_SIL:
+      const filteredNot = state.notlar.filter(
+        (item) => action.payload !== item.id
+      );
+      localStorage.setItem(s10chLocalStorageKey, JSON.stringify(filteredNot));
+      return {
+        ...state,
+        notlar: filteredNot,
+      };
+    case BASLANGIC_DEGER:
+      return {
+        ...state,
+        notlar: baslangicNotlariniGetir(s10chLocalStorageKey),
+      };
+    default:
+      return state;
   }
 }
